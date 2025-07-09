2774f04771e26588702fa0289aab722cc2455bfa
Astro-Coco <colinc131@gmail.com>
2025-07-09 00:07:03 -0400

    corrected positions

--- dev_ws/src/py_avoid/py_avoid/odom.py
+++ dev_ws/src/py_avoid/py_avoid/odom.py
@@ -5,13 +5,14 @@ from rclpy.node import Node
 from nav_msgs.msg import Odometry
 from zenmav.core import Zenmav
 import numpy as np
+import time
 
 from tf_transformations import (
-    quaternion_multiply, quaternion_about_axis
+    quaternion_multiply, quaternion_about_axis, quaternion_conjugate, quaternion_matrix
 )
 
-# constant rotation:  -90° about +Z  (X→Y , Y→–X)
-ROT_Z_NEG90 = quaternion_about_axis(-np.pi/2, (0, 0, 1))
+
+ROT_Z_POS90 = quaternion_about_axis(np.pi/2, (0, 0, 1))
 
 class LaserOdomSubscriber(Node):
 
@@ -23,24 +24,42 @@ class LaserOdomSubscriber(Node):
             self.odom_callback,
             10)
         self.get_logger().info('Subscribed to /laser_odometry')
-        #nav = Zenmav(ip = '127.0.0.1:14551')
+        nav = Zenmav(ip = '127.0.0.1:14552')
+
+        # latitude and longitude must be integers in 1e7 deg, altitude in millimeters
+        lat_deg = 45.1234567
+        lon_deg = -73.1234567
+        alt_m   = 150.0
+        ts        = nav.connection.target_system                 # ubyte
+        lat_i     = int(lat_deg  * 1e7)                  # int32
+        lon_i     = int(lon_deg  * 1e7)                  # int32
+        alt_i     = int(alt_m    * 1000)                 # int32 (mm)
+        time_usec = int(time.time() * 1e6)               # uint64
+
+        nav.connection.mav.set_gps_global_origin_send(
+            ts,
+            lat_i,
+            lon_i,
+            alt_i,
+            time_usec
+        )
 
         self.publisher = self.create_publisher(Odometry, '/mavros/odometry/out', 10)
-        pub_Freq = 15 
-        self.pub_timer = self.create_timer(1/pub_Freq, self.publish_odometry)
         self.pose = None
 
     def publish_odometry(self):
         if self.pose is not None:
             msg = Odometry()
             msg.header.stamp = self.get_clock().now().to_msg()
-            msg.header.frame_id = 'odom'
+            msg.header.frame_id = 'map'
             msg.child_frame_id = 'base_link'
             
             # Fill in the position
-            msg.pose.pose.position.x = self.py
-            msg.pose.pose.position.y =  -self.px
+            msg.pose.pose.position.x = self.px
+            msg.pose.pose.position.y =  self.py
             msg.pose.pose.position.z = self.pz
+            #self.get_logger().info(f'Publishing position: x={msg.pose.pose.position.x}, y={msg.pose.pose.position.y}, z={msg.pose.pose.position.z}')
+            
 
             # Fill in the linear velocity
             msg.twist.twist.linear.x = self.lvy
@@ -51,7 +70,9 @@ class LaserOdomSubscriber(Node):
             # Fill in the orientation (quaternion)
             q_in = np.array([self.ox, self.oy, self.oz, self.ow])
 
-            q_ros = quaternion_multiply(q_in, ROT_Z_NEG90)
+            q_ros = quaternion_multiply(q_in, ROT_Z_POS90)
+            q_ros /= np.linalg.norm(q_ros)
+
 
 
             msg.pose.pose.orientation.x = q_ros[0]
@@ -61,10 +82,10 @@ class LaserOdomSubscriber(Node):
             
 
 
-            sig_pos_xy  = 0.03          # m
-            sig_pos_z   = 0.03
-            sig_ang_rp  = float(np.deg2rad(5))
-            sig_ang_yaw = float(np.deg2rad(3))
+            sig_pos_xy  = 0.05        # m
+            sig_pos_z   = 0.05
+            sig_ang_rp  = float(np.deg2rad(3))
+            sig_ang_yaw = float(np.deg2rad(5))
 
             pose_cov = [
                 float(sig_pos_xy**2), 0.0, 0.0, 0.0, 0.0, 0.0,
@@ -78,8 +99,8 @@ class LaserOdomSubscriber(Node):
 
             msg.pose.covariance = pose_cov
 
-            sig_speed_xy  = 0.05    # metres
-            sig_speed_z   = 0.05
+            sig_speed_xy  = 0.1    # metres
+            sig_speed_z   = 0.1
             sig_speed_ang_rp  = 0.1
             sig_speed_ang_yaw = 0.1
 
@@ -90,7 +111,7 @@ class LaserOdomSubscriber(Node):
                 0.0, 0.0, 0.0, float(sig_speed_ang_rp**2), 0.0, 0.0,
                 0.0, 0.0, 0.0, 0.0, float(sig_speed_ang_rp**2), 0.0,
                 0.0, 0.0, 0.0, 0.0, 0.0, float(sig_speed_ang_yaw**2)
-            ]'''
+            ]
 
             
 
@@ -130,6 +151,7 @@ class LaserOdomSubscriber(Node):
         self.lvz = msg.twist.twist.linear.z
         #self.get_logger().info(f'Linear Vel → x: {lvx:.3f}, y: {lvy:.3f}, z: {lvz:.3f}')
 
+        self.publish_odometry()
 
 def main(args=None):
     rclpy.init(args=args)
