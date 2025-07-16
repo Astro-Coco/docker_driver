9c5f33f36c72d8642f08317ab0e04458c8a96bca
Astro-Coco <colinc131@gmail.com>
2025-07-16 09:28:30 -0400

    added good transforms and fix relay node

--- dev_ws/src/bringup/launch/pc2_to_scan.launch.py
+++ dev_ws/src/bringup/launch/pc2_to_scan.launch.py
@@ -16,16 +16,37 @@ def generate_launch_description():
             description='Whether to launch the pointcloud_to_laserscan node'
         ),
 
-        # static transform from map → base_link
         Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_map_to_base_link',
-            arguments=[
-                '0', '0', '0',  '0', '0', '0', '1',
-                'map', 'base_link'
+            package    = 'tf2_ros',
+            executable = 'static_transform_publisher',
+            name       = 'static_sensor_to_base_link',
+            # each CLI token **must** be its own string
+            arguments  = [
+                '0.0', '0.0', '0.05',          # x y z
+                '-1.570796',   '0.0',   '0.0',     # yaw pitch roll
+                'sensor', 'base_link'          # parent  child
             ],
+            output = 'screen',                 # optional: pipe logs to terminal
+            # arguments += ['--period', '0.05']  # uncomment → re-broadcast at 20 Hz
         ),
+
+
+        Node(
+            package='py_avoid',
+            executable='odom',      # same token you use with ros2 run
+            name='odom',
+            output='screen',
+            # parameters=[...]                # add params if the node accepts any
+        ),
+
+        Node(
+            package='py_avoid',
+            executable='scan_to_mavlink',      # same token you use with ros2 run
+            name='scan_to_mavlink',
+            output='screen',
+            # parameters=[...]                # add params if the node accepts any
+        ),
+
 
         # pointcloud_to_laserscan, only if use_scan == true
         Node(
@@ -38,16 +59,16 @@ def generate_launch_description():
                 ('scan',    '/scan'),
             ],
             parameters=[{
-                'target_frame':       'base_link',
+                'target_frame':       'sensor',
                 'transform_tolerance': 0.1,
                 'min_height':         0.0,
-                'max_height':         1.0,
+                'max_height':         0.1,
                 'angle_min':         -3.141592,
                 'angle_max':          3.141592,
                 'angle_increment':    0.0087,
-                'scan_time':          0.3333,
-                'range_min':          0.20,
-                'range_max':          4.0,
+                'scan_time':          0.2,
+                'range_min':          0.3,
+                'range_max':          20.0,
                 'use_inf':            True,
                 'inf_epsilon':        1.0,
             }],
